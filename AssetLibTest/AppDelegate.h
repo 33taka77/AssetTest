5054aae6b488192b1968e677578331e41a5d6f04
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-03-01 20:18:10 +0900

    Initial Commit

---
+++ AssetLibTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AssetLibTest
+//
+//  Created by 相澤 隆志 on 2014/03/01.
+//  Copyright (c) 2014年 相澤 隆志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
