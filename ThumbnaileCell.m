ef5f38b7545ed2061b23366c0610ce632d054311
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-03-02 23:14:25 +0900

    thumbnail のUI置いただけ

---
+++ ThumbnaileCell.m
@@ -0,0 +1,31 @@
+//
+//  ThumbnaileCell.m
+//  AssetLibTest
+//
+//  Created by 相澤 隆志 on 2014/03/02.
+//  Copyright (c) 2014年 相澤 隆志. All rights reserved.
+//
+
+#import "ThumbnaileCell.h"
+
+@implementation ThumbnaileCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
