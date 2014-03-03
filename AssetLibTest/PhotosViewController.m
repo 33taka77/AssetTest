01d82e6ac359ad26ced9074af8042038bcaf40d6
33taka77 <aizawa-2@sky.bbexcite.jp>
2014-03-02 23:50:28 -0800

    Dataの持ち方変更でも落ちる

--- AssetLibTest/PhotosViewController.m
+++ AssetLibTest/PhotosViewController.m
@@ -15,7 +15,7 @@
 @interface PhotosViewController ()
 @property (weak, nonatomic) IBOutlet UICollectionView *photoBaseCollection;
 @property (nonatomic, retain) NSMutableArray* dateEntry;
-@property (nonatomic, retain) NSMutableArray* photoInfoArray;
+//@property (nonatomic, retain) NSMutableArray* photoInfoArray;
 //@property (nonatomic, retain) NSMutableArray* displayItems;
 @end
 
@@ -31,12 +31,27 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-        self.photoInfoArray = nil;
+//        self.photoInfoArray = nil;
         self.dateEntry = nil;
     }
     return self;
 }
 
+- (void)buildSectionsForDate
+{
+    for( ALAsset* asset in self.targetGroupObj.assets )
+    {
+        NSDictionary* dict = [[asset defaultRepresentation] metadata];
+        NSDictionary* exifData = [self getPhotoExifMetaData:dict];
+        NSString* date = exifData[@"DateTimeOriginal"];
+        [self rebuildForDate:date asset:asset];
+        dict = nil;
+        exifData = nil;
+        date = nil;
+    }
+}
+
+/*
 - (void)buildPhotoDictionary
 {
     if( self.photoInfoArray == nil )
@@ -51,7 +66,46 @@
         [self.photoInfoArray addObject:targetDictionary];
     }
 }
+*/
 
+- (void)rebuildForDate:(NSString*)date asset:(ALAsset*)asset
+{
+    if( self.dateEntry == nil )
+    {
+        self.dateEntry = [[NSMutableArray alloc] init];
+        SectionData* emptySection = [[SectionData alloc] initWithTitle:@"Unknown"];
+        [self.dateEntry addObject:emptySection];
+    }
+    NSArray* strs = [date componentsSeparatedByString:@" "];
+    NSString* title = strs[0];
+    NSURL* url = [asset valueForProperty:ALAssetPropertyAssetURL];
+       
+    if( title == nil )
+    {
+        title = @"Unknown";
+    }
+    BOOL isNewItem = YES;
+    for( SectionData* section in self.dateEntry )
+    {
+        if( [section.sectionTitle isEqual:title] )
+        {
+            [section.items addObject:url];
+            isNewItem = NO;
+            break;
+        }
+    }
+    if( isNewItem == YES )
+    {
+        SectionData* newSection = [[SectionData alloc] initWithTitle:title];
+        [newSection.items addObject:url];
+        [self.dateEntry addObject:newSection];
+    }
+        //NSLog(@"title: %@ sub:%@",strs[0],strs[1]);
+    strs = nil;
+    title = nil;
+    url = nil;
+}
+/*
 - (void)rebuildForDate
 {
     if( self.dateEntry ==nil )
@@ -91,6 +145,8 @@
         //NSLog(@"title: %@ sub:%@",strs[0],strs[1]);
     }
 }
+*/
+
 - (id)initWitheGroupName:(NSString*)name
 {
     self = [super init];
@@ -106,10 +162,20 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.navigationItem.title = self.groupName;
-    [self buildPhotoDictionary];
-    [self rebuildForDate];
+    [self buildSectionsForDate];
 }
 
+- (void)viewWillDisappear:(BOOL)animated
+{
+    for( SectionData* section in self.dateEntry )
+    {
+        [section.items removeAllObjects];
+        section.items = nil;
+    }
+    [self.dateEntry removeAllObjects];
+    self.dateEntry = nil;
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
